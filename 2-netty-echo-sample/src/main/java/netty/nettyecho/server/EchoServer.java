0df3e3e18feee54fafce5e25d7ca95d3fa52429c
aesperer <piyrw9754@gmail.com>
2023-08-22 15:14:34 +0900

    start handler

--- 2-netty-echo-sample/src/main/java/netty/nettyecho/server/EchoServer.java
+++ 2-netty-echo-sample/src/main/java/netty/nettyecho/server/EchoServer.java
@@ -1,5 +1,15 @@
 package netty.nettyecho.server;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
 public class EchoServer {
 
     private final int port;
@@ -10,11 +20,30 @@ public class EchoServer {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        new EchoServer(5001).start();
+        new EchoServer(8888).start();
 
     }
 
     private void start() throws InterruptedException {
+        final EchoServerHandler handler = new EchoServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
 
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(handler);
+                        }
+                    });
+            ChannelFuture f = b.bind().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
     }
 }
