bdeda332ab0e30b187ae663b706a72504bdc0074
aesperer <piyrw9754@gmail.com>
2023-08-22 15:23:39 +0900

    echo client

---
+++ 2-netty-echo-sample/src/main/java/netty/nettyecho/client/EchoClient.java
@@ -0,0 +1,44 @@
+package netty.nettyecho.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import netty.nettyecho.server.EchoServerHandler;
+
+import java.net.InetSocketAddress;
+
+public class EchoClient {
+
+    public EchoClient() {
+    }
+
+    public static void main(String[] args) throws Exception {
+        new EchoClient().start();
+    }
+
+    public void start() throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(8888))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new EchoServerHandler());
+                        }
+                    });
+            ChannelFuture f = b.connect().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+}
